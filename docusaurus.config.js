56d9e8c3c92b94be02ef8eed4b93f2901cb13b1b
ji-one <ds020228@naver.com>
2023-12-09 00:43:00 +0900

    Modify github url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/ji-one",
             label: "GitHub",
             position: "right",
           },
